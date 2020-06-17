d10aa84ee19b7fb78d9a335401fb552b04a1c03b
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-17 13:14:40 +0200

    Added ChatController & Mensaje

---
+++ src/main/java/com/bolsadeideas/springboot/backend/chat/controllers/ChatController.java
@@ -0,0 +1,24 @@
+package com.bolsadeideas.springboot.backend.chat.controllers;
+
+import java.util.Date;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.bolsadeideas.springboot.backend.chat.models.documents.Mensaje;
+
+@Controller
+public class ChatController {
+	
+	@MessageMapping("/mensaje")
+	@SendTo("/chat/mensaje")
+	public Mensaje recibeMensaje(Mensaje mensaje) {
+		
+		mensaje.setFecha( new Date().getTime() );
+		mensaje.setTexto("REcibido por el Broker: " + mensaje.getTexto());
+		
+		return mensaje;
+		
+	}
+}
